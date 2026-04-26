630976dd9b690e0b16d4383298e08f6f8efd9075
Mikalai <kolyaleshiov@gmail.com>
2026-04-26 21:52:39 +0300

    chore: Securing the Main Plugin File

--- wp-content/plugins/udemy-plus/index.php
+++ wp-content/plugins/udemy-plus/index.php
@@ -11,3 +11,9 @@
  * Text Domain:       udemy-plus
  * Domain Path:       /languages
  */
+
+
+if(!function_exists('add_action')){
+    echo "Seems like you lost. It's plugin's file.";
+    exit;
+}
