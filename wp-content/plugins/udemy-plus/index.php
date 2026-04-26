b26ad0662505f786519a7ef8762d237da228021f
Mikalai <kolyaleshiov@gmail.com>
2026-04-26 21:42:35 +0300

    feat: init custom plugin for blocks

---
+++ wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://github.com/NikolayLeshchev/FSE-WP-theme-Course
+ * Description:       A plugin for adding blocks to the theme.
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            Mikalai Liashchou
+ * Author URI:        https://www.linkedin.com/in/nikolayleshchev/
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
