a79bed1f949b530a3885b041d5a954101af581c0
Mikalai <kolyaleshiov@gmail.com>
2026-05-09 23:03:18 +0300

    feat: create Fancy header Gutenberg block

---
+++ wp-content/plugins/udemy-plus/src/index.js
@@ -0,0 +1,40 @@
+import { registerBlockType } from '@wordpress/blocks'
+import { RichText, useBlockProps } from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
+import block from './block.json'
+import './main.css'
+
+registerBlockType(block.name, {
+    edit({attributes, setAttributes}){
+        const {content} = attributes
+        const blockProps = useBlockProps()
+
+        return (
+            <div {...blockProps}>
+               <RichText 
+                    tagName="h2" 
+                    className="fancy-header"
+                    placeholder={__('Enter heading', 'udemy-plus')}
+                    value={content}
+                    onChange={newVal => setAttributes({ content: newVal })}
+                    allowedFormats={['core/bold', 'core/italic']}
+                /> 
+            </div>
+            
+        )
+    },
+    save({attributes}){
+        const {content} = attributes
+        const blockProps = useBlockProps.save({
+            className: "fancy-header"
+        })
+
+        return (
+            <RichText.Content
+                {...blockProps}
+                tagName="h2" 
+                value={content}
+            />
+        )
+    }
+})
