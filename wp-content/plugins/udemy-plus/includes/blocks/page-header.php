1cd138c8c5ffb68bdad9aa6b24113ffc52a22c16
Mikalai <kolyaleshiov@gmail.com>
2026-05-23 21:47:07 +0300

    feat: create page header block and update category page template

---
+++ wp-content/plugins/udemy-plus/includes/blocks/page-header.php
@@ -0,0 +1,23 @@
+<?php
+
+function up_page_header_render_cb($attr) {
+    $heading = esc_html($attr['content']);
+
+    if($attr['showCategory']){
+        $heading = get_the_archive_title();
+    }
+
+    ob_start(); ?>
+
+    <div class="wp-block-udemy-plus-page-header">
+        <div class="inner-page-header">
+            <h1><?php echo $heading; ?></h1>
+        </div>
+    </div>
+
+    <?php
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
