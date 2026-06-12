dd92c0ede067245723dc5db670a03cc13f0b1a3d
Mikalai Liashchou <kolyaleshiov@gmail.com>
2026-06-12 23:31:53 +0300

    feat: begin work on recipe summury frontend output

--- wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/recipe-summary/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '7a2bc5316b4373e50bf3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'ccb0aee822fb8aef14d9');
