884d63bbbcb3d75014a557685fe4fccf83d7b6a1
Mikalai Liashchou <kolyaleshiov@gmail.com>
2026-06-28 12:53:08 +0300

    refactor: separete index.js into edit and save files

--- wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '97f723885fb9a30faf28');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '613c78310de09d383c5f');
