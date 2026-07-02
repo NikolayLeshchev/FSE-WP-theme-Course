45cca9e580207b9c9f1ed4c79f6e4821f250901f
Mikalai Liashchou <kolyaleshiov@gmail.com>
2026-07-02 23:44:58 +0300

    feat: add post query logic in Popular Recipes block

--- wp-content/plugins/udemy-plus/build/blocks/popular-recipes/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/popular-recipes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e882c5fec39c5661f834');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f481f4fb6f1858c6b6d0');
--- wp-content/plugins/udemy-plus/includes/blocks/popular-recipes.php
+++ wp-content/plugins/udemy-plus/includes/blocks/popular-recipes.php
@@ -1,6 +1,31 @@
 <?php
 
-function up_popular_recipes_cb() {
+function up_popular_recipes_cb($atts) {
+    $title = esc_html($atts['title']);
+    $cuisineIDs = array_map(function ($term) {
+        return $term['id'];
+    }, $atts['cuisines']);
+
+
+    $args = [
+        'post_type' => 'recipe',
+        'post_per_page' => $atts['count'],
+        'order_by' => 'meta_value_num',
+        'meta_key' => 'recipe_rating',
+        'order' => 'DESC'
+    ];
+
+    if(!empty($cuisineIDs)){
+        $args['tax_query'] = [
+            [
+                'taxonomy' => 'cuisine',
+                'field' => 'term_id',
+                'terms' => $cuisineIDs
+            ]
+        ];
+    }
+    $query = new WP_Query($args);
+
     ob_start();
     ?>
 
