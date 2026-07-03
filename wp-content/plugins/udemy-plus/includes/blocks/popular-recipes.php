b9e3f290430436af325eef5b8d80ce8b039ef06f
Mikalai Liashchou <kolyaleshiov@gmail.com>
2026-07-03 18:54:54 +0300

    feat: complete Popular Recipes block

--- wp-content/plugins/udemy-plus/includes/blocks/popular-recipes.php
+++ wp-content/plugins/udemy-plus/includes/blocks/popular-recipes.php
@@ -9,8 +9,8 @@ function up_popular_recipes_cb($atts) {
 
     $args = [
         'post_type' => 'recipe',
-        'post_per_page' => $atts['count'],
-        'order_by' => 'meta_value_num',
+        'posts_per_page' => $atts['count'],
+        'orderby' => 'meta_value_num',
         'meta_key' => 'recipe_rating',
         'order' => 'DESC'
     ];
@@ -29,8 +29,37 @@ function up_popular_recipes_cb($atts) {
     ob_start();
     ?>
 
+    <div class="wp-block-udemy-plus-popular-recipes">
+        <h6><?php echo $title; ?></h6>
+
+        <?php
+            if($query->have_posts()){
+                while($query->have_posts()){
+                    $query->the_post();
+                    ?>
+                    <div class="single-post">
+                        <a href="<?php the_permalink(); ?>" class="single-post-image">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </a>
+                        <div class="single-post-detail">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+                            <span>
+                                By <a href="<?php the_permalink(); ?>">
+                                        <?php the_author(); ?>
+                                    </a>
+                            </span>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+        ?>
+    </div>
 
     <?php
+    wp_reset_postdata();
 
     $output = ob_get_contents();
     ob_end_clean();
