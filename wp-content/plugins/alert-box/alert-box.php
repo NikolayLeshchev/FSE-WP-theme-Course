82267517ea2e7ab619ce1d0b157c912c9d0106b7
Mikalai Liashchou <kolyaleshiov@gmail.com>
2026-07-04 14:38:59 +0300

    feat: create new Alert Box plugin with npx

---
+++ wp-content/plugins/alert-box/alert-box.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Alert Box
+ * Description:       Adds an alert box to output information to the reader.
+ * Version:           0.1.0
+ * Requires at least: 6.8
+ * Requires PHP:      7.4
+ * Author:            Mikalai Liashchou
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       alert-box
+ *
+ * @package UPlus
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+/**
+ * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
+ * based on the registered block metadata. Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
+ * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
+ */
+function u_plus_alert_box_block_init() {
+	wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+}
+add_action( 'init', 'u_plus_alert_box_block_init' );
---
+++ wp-content/plugins/alert-box/build/blocks-manifest.php
@@ -0,0 +1,40 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'alert-box' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'u-plus/alert-box',
+		'version' => '0.1.0',
+		'title' => 'Alert Box',
+		'category' => 'widgets',
+		'description' => 'Adds an alert box to output important information to the reader.',
+		'icon' => 'welcome-learn-more',
+		'attributes' => array(
+			'content' => array(
+				'type' => 'string',
+				'source' => 'html',
+				'selector' => '.alert-box-content p'
+			),
+			'icon' => array(
+				'type' => 'string',
+				'default' => 'admin-site'
+			),
+			'bgColor' => array(
+				'type' => 'string',
+				'default' => '#4F46E5'
+			),
+			'textColor' => array(
+				'type' => 'string',
+				'default' => '#fff'
+			)
+		),
+		'textdomain' => 'alert-box',
+		'editorScript' => 'file:./index.js',
+		'style' => 'file:./style-index.css',
+		'supports' => array(
+			'html' => false,
+			'align' => true
+		)
+	)
+);
